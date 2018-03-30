e7f319a23f5a433722a17cc27f93c24b870abb22
Christopher K <crsk@mail.com>
2018-03-29 21:01:41 -0300

    segundo commmit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { VideosComponent } from './videos/videos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  {
+    path: 'videos',
+    component: VideosComponent,
+    data: { title: 'Videos' }
+  },
+  { path: '',
+    redirectTo: '/inicio',
+    pathMatch: 'full'
+  },
+  { path: '**', component: InicioComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { VideosComponent } from './videos/videos.component';
 
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+
+//bootstrap
+import { AlertModule } from 'ngx-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    VideosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
